12f9ed168af618be817a5958649f2ae3724e78da
hufeia <Hufei13579>
2020-01-21 15:25:40 +0800

    update

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+const chalk = require('chalk');
+const log = console.log;
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.redirect('index.html');
 });
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!\n');
-  console.log('http://localhost:3000');
+  log(chalk.green('Example app listening on port 3000!\n'));
+  log(chalk.blue.underline.bold('http://localhost:3000\n'));
 });
