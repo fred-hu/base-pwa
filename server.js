1a62d5076c08765344490d1998fd4b90ee6df4a3
fred-hu <hufei_yx@sina.com>
2020-12-22 14:19:57 +0800

    📦 https

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-var express = require('express');
-const chalk = require('chalk');
-const log = console.log;
-const app = express();
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-  res.redirect('index.html');
-});
-app.listen(3000, function() {
-  log(chalk.green('Example app listening on port 3000!\n'));
-  log(chalk.blue.underline.bold('http://localhost:3000\n'));
-});
+var express = require('express')
+var fs = require('fs')
+var https = require('https')
+const chalk = require('chalk')
+const log = console.log
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.get('/', function (req, res) {
+  res.redirect('index.html')
+})
+https
+  .createServer(
+    {
+      key: fs.readFileSync('server.key'),
+      cert: fs.readFileSync('server.cert')
+    },
+    app
+  )
+  .listen(3000, function () {
+    log(chalk.green('Example app listening on port 3000!\n'))
+    log(chalk.blue.underline.bold('https://localhost:3000\n'))
+  })
